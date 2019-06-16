9a8758cfb2158b8c8c32f646bc158d3704b41085
Daniel Marques <daniel.dms.23@GMAIL.COM>
2019-06-16 16:14:04 -0300

    Feed list from server

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import api from '../services/api';
+
 import './Feed.css';
 
 import more from '../assets/more.svg';
@@ -8,57 +10,44 @@ import send from '../assets/send.svg';
 import comment from '../assets/comment.svg';
 
 export default class Feed extends Component {
+    state = {
+        feeds: [],
+    };
+     
+    async componentDidMount() {
+        const response = await api.get('posts');
+
+        this.setState({feeds: response.data});
+    }
     render() {
         return (
             <section id="post-list"> 
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Daniel Marques</span>
-                            <span className="place">João Pessoa</span>
-                        </div>
-                        <img src={more} alt="Mais opções"/>
-                    </header>
-                    <img src="http://localhost:3333/files/bored-manda-te-700x360.jpg" />
-                    <footer>
-                        <div className="actions">
-                            <img src={like} />
-                            <img src={comment} />
-                            <img src={send} />
-                        </div>
+                {this.state.feeds.map(post => (
+                    <article key={post._id}>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
+                            <img src={more} alt="Mais opções"/>
+                        </header>
+                        <img src={`http://localhost:3333/files/${post.image}`} />
+                        <footer>
+                            <div className="actions">
+                                <img src={like} />
+                                <img src={comment} />
+                                <img src={send} />
+                            </div>
 
-                        <strong>9000 Curtidas</strong>
+                            <strong>{post.likes} Curtidas</strong>
 
-                        <p>
-                            Post muito massa da oministack
-                            <span className="hashtags">#muito #massa</span>
-                        </p>
-                    </footer>
-                </article>
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Daniel Marques</span>
-                            <span className="place">João Pessoa</span>
-                        </div>
-                        <img src={more} alt="Mais opções"/>
-                    </header>
-                    <img src="http://localhost:3333/files/bored-manda-te-700x360.jpg" />
-                    <footer>
-                        <div className="actions">
-                            <img src={like} />
-                            <img src={comment} />
-                            <img src={send} />
-                        </div>
-
-                        <strong>9000 Curtidas</strong>
-
-                        <p>
-                            Post muito massa da oministack
-                            <span className="hashtags">#muito #massa</span>
-                        </p>
-                    </footer>
-                </article>
+                            <p>
+                                {post.description}
+                                <span className="hashtags">{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                ))}
             </section>
         );
     }
